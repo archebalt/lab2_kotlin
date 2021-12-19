dd150bffaf3160b95ecf7750098fa57224b43f2d
Artur <arturha13@gmail.com>
2021-12-19 14:36:10 +0300

    lab2_kotlin

---
+++ lab2/Element.kt
@@ -0,0 +1,10 @@
+package lab2
+
+class Element(_element: String,_elemType: ElemType) {
+    val element: String
+    val elemType: ElemType
+    init {
+        element = _element
+        elemType = _elemType
+    }
+}
